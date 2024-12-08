dc135ffd6b2735a70ebdc19a1cd3ab5ab3b30276
SamirSam <samirsherif70@gmail.com>
2024-12-08 20:19:39 +0200

    Four-in-a-row game with AI player is done

---
+++ Games with AI/Four-in-a-row.h
@@ -0,0 +1,197 @@
+#ifndef _FOUR_IN_A_ROW_H
+#define _FOUR_IN_A_ROW_H
+
+#include"BoardGame_Classes.h"
+
+// Last move
+int row, col;
+char mark;
+vector<vector<char>>marks(6, vector<char>(7, ' '));
+
+template <typename T>
+class FourInARowBoard : public Board<T> {
+private:
+    static int turn;
+    int countRow, countRD, countLD;
+    bool checkWinner(int x, int y, int addRow, int addCol);
+
+public:
+    FourInARowBoard();
+    bool update_board (int x , int y , T symbol);
+    void display_board () ;
+    bool is_win() ;
+    bool is_draw();
+    bool game_is_over();
+};
+template <typename T>
+int FourInARowBoard<T>::turn = 0;
+
+
+template <typename T>
+class FourInARowPlayer : public Player<T> {
+public:
+    FourInARowPlayer(string name, T symbol);
+    void getmove(int& x, int& y);
+};
+
+
+template <typename T>
+class FourInARow_Random_Player : public RandomPlayer<T>{
+public:
+    FourInARow_Random_Player (T symbol);
+    void getmove(int &x, int &y) ;
+};
+
+
+//--------------------------------------- IMPLEMENTATION ----------------------------
+
+template <typename T>
+bool FourInARowBoard<T>::checkWinner(int x, int y, int addRow, int addCol){
+    for (int i = 1; i < 4; ++i) {
+        int idxRow = x - i * addRow, idxCol = y + i * addCol;
+        if (idxRow < 0 || idxRow > 5 || idxCol < 0 || idxCol > 6 || this->board[idxRow][idxCol] != mark)
+            return false;
+
+        if ((addRow == 0 && addCol == 1) || (addRow == 0 && addCol == -1))
+            countRow++;
+        else if ((addRow == 1 && addCol == 1) || (addRow == -1 && addCol == -1))
+            countRD++;
+        else if ((addRow == 1 && addCol == -1) || (addRow == -1 && addCol == 1))
+            countLD++;
+    }
+    return true;
+}
+
+
+// Constructor FourInARowBoard
+template <typename T>
+FourInARowBoard<T>::FourInARowBoard() {
+    this->rows = 6;
+    this->columns = 7;
+    this->board = new char*[this->rows];
+    for (int i = 0; i < this->rows; i++) {
+        this->board[i] = new char[this->columns];
+        for (int j = 0; j < this->columns; j++) {
+            this->board[i][j] = ' ';
+        }
+    }
+    this->n_moves = 0;
+}
+
+
+template <typename T>
+bool FourInARowBoard<T>::update_board(int x, int y, T symbol) {
+    if (y >= 0 && y < this->columns) {
+        if (symbol == ' ') {
+            for (int i = 0; i < this->rows; ++i) {
+                if (this->board[i][y] != symbol) {
+                    this->board[i][y] = symbol;
+                    this->n_moves--;
+                    marks[i][y] = symbol;
+                    return true;
+                }
+            }
+        }
+
+        for (int i = this->rows - 1; i >= 0; i--) {
+            if (this->board[i][y] == ' ') {
+                this->board[i][y] = symbol;
+                this->n_moves++;
+                row = i;
+                col = y;
+                mark = symbol;
+                marks[row][col] = symbol;
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+// Display the board and the pieces on it
+template <typename T>
+void FourInARowBoard<T>::display_board() {
+    turn++;
+
+    if (turn % 2)
+        cout << string(43, '-') << "\n\t       Turn: Player 1\n" << string(43, '-') << "\n";
+    else
+        cout << string(43, '-') << "\n\t       Turn: Player 2\n" << string(43, '-') << "\n";
+
+    for (int i = 0; i < this->rows; i++) {
+        cout << "\n|";
+        for (int j = 0; j < this->columns; j++) {
+            cout << setw(3) << this->board[i][j] << "  |";
+        }
+        cout << endl << string(43, '-');
+    }
+
+    cout << endl;
+    for (int i = 1; i < 8; ++i) {
+        cout << setw(4) << i << "  ";
+    }
+    cout << endl << endl;
+}
+
+
+// Returns true if there is any winner
+template <typename T>
+bool FourInARowBoard<T>::is_win() {
+    countRow = this->countRD = countLD = 1;
+
+    if (checkWinner(row, col, -1, 0) || checkWinner(row, col, 0, 1) ||
+        checkWinner(row, col, 0, -1) || checkWinner(row, col, 1, 1) ||
+        checkWinner(row, col, -1, -1) || checkWinner(row, col, 1, -1) ||
+        checkWinner(row, col,-1, 1)){
+        return true;
+    }
+
+    if (countRow >= 4 || countRD >= 4 || countLD >= 4)
+        return true;
+
+    return false;
+}
+
+
+template <typename T>
+bool FourInARowBoard<T>::is_draw() {
+    return this->n_moves == 42;
+}
+
+
+template <typename T>
+bool FourInARowBoard<T>::game_is_over() {
+    return is_draw() || is_win();
+}
+
+
+// Constructor for FourInARowPlayer
+template <typename T>
+FourInARowPlayer<T>::FourInARowPlayer(string name, T symbol) : Player<T>(name, symbol) {}
+
+
+template <typename T>
+void FourInARowPlayer<T>::getmove(int& x, int& y) {
+    cout << "\nChoose a column (1 to 7): ";
+    cin >> y;
+    y--;
+    x = 0;
+}
+
+
+template <typename T>
+FourInARow_Random_Player<T>::FourInARow_Random_Player(T symbol) : RandomPlayer<T>(symbol) {
+    this->dimension = 7;
+    this->name = "Random Computer Player";
+    srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
+}
+
+
+template <typename T>
+void FourInARow_Random_Player<T>::getmove(int& x, int& y) {
+    y = rand() % this->dimension;   // Random number between 0 and 6
+}
+
+
+#endif //_FOUR_IN_A_ROW_H
---
+++ Games with AI/Four-in-a-row_MinMaxPlayer.h
@@ -0,0 +1,177 @@
+#ifndef _MINMAXPLAYER_H
+#define _MINMAXPLAYER_H
+
+#include "BoardGame_Classes.h"
+#include "Four-in-a-row.h"
+
+template <typename T>
+class FourInARow_MinMax_Player : public Player<T> {
+public:
+    FourInARow_MinMax_Player(T symbol);
+    void getmove(int& x, int& y) override;
+
+private:
+    int rows, columns, lineLength;
+    pair<int, long long> MinMax(int depth, long long alpha, long long beta, bool isMaximizing);
+    int cellScore();
+    int evaluateLine(vector<T> line);
+};
+
+
+//--------------------------------------- IMPLEMENTATION ----------------------------
+
+
+// Constructor for the templated class
+template <typename T>
+FourInARow_MinMax_Player<T>::FourInARow_MinMax_Player(T symbol) : Player<T>(symbol) {
+    this->name = "AI Player";
+    this->rows = 6;
+    this->columns = 7;
+    this->lineLength = 4;
+}
+
+
+// Method to get the best move for the player
+template <typename T>
+void FourInARow_MinMax_Player<T>::getmove(int& x, int& y) {
+    y = MinMax(5, numeric_limits<long long>::min(), numeric_limits<long long>::max(), true).first;
+}
+
+
+template<typename T>
+int FourInARow_MinMax_Player<T>::evaluateLine(vector<T> line){
+    int score = 0;
+    T enemySymbol = 'X';    // X is the symbol of player (human)
+
+    if (count(line.begin(), line.end(), this->symbol) == 4)
+        score += 100;
+    else if (count(line.begin(), line.end(), this->symbol) == 3 && count(line.begin(), line.end(), ' ') == 1)
+        score += 5;
+    else if (count(line.begin(), line.end(), this->symbol) == 2 && count(line.begin(), line.end(), ' ') == 2)
+        score += 2;
+
+    if (count(line.begin(), line.end(), enemySymbol) == 3 && count(line.begin(), line.end(), ' ') == 1)
+        score -= 4;
+
+    return score;
+}
+
+
+template <typename T>
+int FourInARow_MinMax_Player<T>::cellScore() {
+    int score = 0;
+
+    // Score center column
+    vector<T> midCol;
+    for (int i = rows - 1; i >= 0; --i)
+        midCol.push_back(marks[i][3]);
+    score += 3 * count(midCol.begin(), midCol.end(), this->symbol);   // May be cut to evaluateLine
+
+    // Score Horizontal
+    for (int i = rows - 1; i >= 0; --i) {
+        vector<T> row;
+        for (int j = 0; j < columns; ++j)
+            row.push_back(marks[i][j]);
+
+        for (int j = 0; j < columns - 3; ++j) {
+            vector<T> line(row.begin() + j, row.begin() + j + lineLength);
+            score += evaluateLine(line);
+        }
+    }
+
+    // Score Vertical
+    for (int i = 0; i < columns; ++i) {
+        vector<T> col;
+        for (int j = rows - 1; j >= 0; --j)
+            col.push_back(marks[j][i]);
+
+        for (int j = 0; j < rows - 3; ++j) {
+            vector<T> line(col.begin() + j, col.begin() + j + lineLength);
+            score += evaluateLine(line);
+        }
+    }
+
+    // Score right diagonal
+    for (int i = rows - 1; i > rows - 4; --i) {
+        for (int j = 0; j < columns - 3; ++j) {
+            vector<T> line;
+            for (int k = 0; k < lineLength; ++k) {
+                line.push_back(marks[i - k][j + k]);
+            }
+            score += evaluateLine(line);
+        }
+    }
+
+    // Score left diagonal
+    for (int i = rows - 1; i > rows - 4; --i) {
+        for (int j = 0; j < columns - 3; ++j) {
+            vector<T> line;
+            for (int k = 0; k < lineLength; ++k) {
+                line.push_back(marks[i - 3 + k][j + k]);
+            }
+            score += evaluateLine(line);
+        }
+    }
+
+    return score;
+}
+
+
+template<typename T>
+pair<int, long long>FourInARow_MinMax_Player<T>::MinMax(int depth, long long alpha, long long beta, bool isMaximizing){
+    if (!isMaximizing && this->boardPtr->is_win())  // If AI won
+        return { 0, 10000000000000 };
+
+    else if (this->boardPtr->is_win())  // If human won
+        return { 0, -10000000000000 };
+
+    else if (this->boardPtr->is_draw())
+        return { 0, 0};
+
+    else if (depth == 0)
+        return {0, cellScore()};
+
+    if (isMaximizing){
+        long long bestScore = numeric_limits<long long>::min();
+        int column;
+        for (int col = 0; col < columns; ++col) {
+            if (this->boardPtr->update_board(0, col, 'O')){
+                long long score = MinMax(depth - 1, alpha, beta, false).second;
+                this->boardPtr->update_board(0, col, ' ');
+                if (score > bestScore){
+                    bestScore = score;
+                    column = col;
+                }
+                alpha = max(alpha, bestScore);
+                if (alpha >= beta)
+                    break;
+            }
+        }
+        return {column, bestScore} ;
+    }
+
+    else {  // Minimizing player
+        long long worstScore = numeric_limits<long long>::max();
+        int column;
+        for (int col = 0; col < columns; ++col) {
+            if (this->boardPtr->update_board(0, col, 'X')){
+                long long score = MinMax(depth - 1, alpha, beta,true).second;
+                this->boardPtr->update_board(0, col, ' ');
+                if (score < worstScore){
+                    worstScore = score;
+                    column = col;
+                }
+                beta = min(beta, worstScore);
+                if (alpha >= beta)
+                    break;
+            }
+        }
+        return {column, worstScore} ;
+    }
+}
+
+
+
+
+
+#endif //_MINMAXPLAYER_H
