a9003cc199ecc19def71d48369833864bb105bb8
samir-1-sam <162159525+samir-1-sam@users.noreply.github.com>
2024-12-08 21:24:36 +0200

    Update Four-in-a-row_MinMaxPlayer.h

--- Games with AI/Four-in-a-row_MinMaxPlayer.h
+++ Games with AI/Four-in-a-row_MinMaxPlayer.h
@@ -56,6 +56,7 @@ int FourInARow_MinMax_Player<T>::evaluateLine(vector<T> line){
 }
 
 
+// evaluate the board
 template <typename T>
 int FourInARow_MinMax_Player<T>::cellScore() {
     int score = 0;
@@ -116,6 +117,7 @@ int FourInARow_MinMax_Player<T>::cellScore() {
 }
 
 
+// Find the best move using the minimax algorithm
 template<typename T>
 pair<int, long long>FourInARow_MinMax_Player<T>::MinMax(int depth, long long alpha, long long beta, bool isMaximizing){
     if (!isMaximizing && this->boardPtr->is_win())  // If AI won
@@ -155,7 +157,7 @@ pair<int, long long>FourInARow_MinMax_Player<T>::MinMax(int depth, long long alp
         for (int col = 0; col < columns; ++col) {
             if (this->boardPtr->update_board(0, col, 'X')){
                 long long score = MinMax(depth - 1, alpha, beta,true).second;
-                this->boardPtr->update_board(0, col, ' ');
+                this->boardPtr->update_board(0, col, ' ');   // Undo move
                 if (score < worstScore){
                     worstScore = score;
                     column = col;
